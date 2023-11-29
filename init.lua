b17ff1158ed4f7deec34412225da0b9938a2dd63
vasant paradissa <vasantpns@gmail.com>
2023-11-29 14:32:33 +0700

    disable diagnostics for .env files

--- init.lua
+++ init.lua
@@ -6,6 +6,14 @@ return {
     },
   },
   polish = function()
+    local group = vim.api.nvim_create_augroup("__env", {clear=true})
+    vim.api.nvim_create_autocmd("BufEnter", {
+      pattern = "*.env",
+      group = group,
+      callback = function(args)
+        vim.diagnostic.disable(args.buf)
+      end
+    })
     vim.cmd('colorscheme flexoki-dark')
     vim.opt.clipboard = "unnamedplus"
     if vim.fn.has('wsl') == 1 then
